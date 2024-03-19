2c03c7830713e05a9896a2fc78eca1b121fbb5e5
Angelos Theodoridis <72686781+Agtheo09@users.noreply.github.com>
2024-03-19 19:38:10 +0200

    ExternalIMUTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ExternalIMUTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class ExternalIMUTest extends LinearOpMode {
+    private IMU imu;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        imu = hardwareMap.get(IMU.class, "external_imu");
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.UP;
+
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        IMU.Parameters parameters = new IMU.Parameters(orientationOnRobot);
+
+        imu.initialize(parameters);
+        imu.resetYaw();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            telemetry.addData("Yaw: ", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+            telemetry.update();
+        }
+    }
+}
